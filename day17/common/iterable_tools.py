50b2d0ee4a7812328489ec8ddf61dcdd56d9951c
zjkliuzy <zjkliuzy@live.cn>
2020-11-26 09:46:54 +0800

    代码提交

--- day17/common/iterable_tools.py
+++ day17/common/iterable_tools.py
@@ -1,10 +1,20 @@
+"""
+    可迭代对象工具集
+"""
+
+
 class IterableHelper:
+    """
+        可迭代对象助手
+    """
+
+    # 静态方法:常用的工具,独立性强,不依赖于实例成员和类成员
     @staticmethod
     def find_all(iterable, func):
         """
-        根据条件查找元素
-        :param iterable:
-        :param func:函数类型的条件
+            在可迭代对象中,根据条件查找多个元素
+        :param iterable: 可迭代对象
+        :param func: 函数类型的条件
         :return: 生成器
         """
         for item in iterable:
@@ -14,10 +24,10 @@ class IterableHelper:
     @staticmethod
     def find_single(iterable, func):
         """
-        从列表里面找到一个结果
-        :param iterable:
+            在可迭代对象中,根据条件查找单个元素
+        :param iterable: 可迭代对象
         :param func: 函数类型的条件
-        :return: 结果
+        :return: 满足条件的对象
         """
         for item in iterable:
             if func(item):
@@ -31,31 +41,13 @@ class IterableHelper:
         :param func:
         :return:
         """
-        for emp in iterable:
-            yield func(emp)
+        for item in iterable:
+            yield func(item)
 
     @staticmethod
-    def get_max(func, iterable):
-        """
-
-        :param func:
-        :param iterable:
-        :return:
-        """
+    def get_max(iterable, func):
         max_value = iterable[0]
         for i in range(1, len(iterable)):
             if func(max_value) < func(iterable[i]):
                 max_value = iterable[i]
         return max_value
-    @staticmethod
-    def get_count(func, iterable, money):
-        """
-
-        :param func:
-        :param iterable:
-        :param money:
-        :return:
-        """
-        for item in iterable:
-            if func(item) > money:
-                yield item
